fc5d74d5b6e6351fd6b7e83573a66092cc2aa254
Timur <offershow4531@gmail.com>
2022-04-23 17:55:52 +0600

    WorkVersion_Without Task 5

--- CRMTaskConsole/PassiveEmployee.cs
+++ CRMTaskConsole/PassiveEmployee.cs
@@ -6,46 +6,22 @@ using System.Threading.Tasks;
 
 namespace CRMTaskConsole
 {
-    public  class PassiveEmployee : Employee
+    
+    public class PassiveEmployee 
     {
+
         public string Adress { get; set; }
         public string PassiveName { get; set; }
 
-        public string PhysicFaceOne { get; set; }
+        public Dictionary<string, ActiveEmployee> EmployeeNames { get; set; }
+        
+        public List<string> lists { get; set; } 
+        
+        
 
-        public string PhysicFaceTwo { get; set; }
-        public string PhysicFaceThree { get; set; }
-      
-
-       /* public void Launch(string Name, string Adress)
-        {
-            PassiveInfo = PassiveInfo.getInstance(Name, Adress);
-        }*/
     }
-    /*class PassiveInfo
-    {
-        private static PassiveInfo instance;
 
-        public string BuilderName { get; private set; }
-        public string Adress { get; private set; }
-
-        protected PassiveInfo(string name, string adress)
-        {
-            this.BuilderName = name;
-            this.Adress = adress;
-            
-        }
-
-        public static PassiveInfo getInstance(string name, string adress)
-        {
-            if (instance == null)
-                instance = new PassiveInfo(name, adress);
-            return instance;
-        }
-    }
-    */
-
-
+   
 }
    
 
